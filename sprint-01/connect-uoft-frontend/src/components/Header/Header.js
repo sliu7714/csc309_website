837f9853024c958135737a62b4006fd19e25fa8b
IanRamotar <57780992+IanRamotar@users.noreply.github.com>
2021-11-03 09:37:09 -0400

    implemented create functionality across all pages

--- sprint-01/connect-uoft-frontend/src/components/Header/Header.js
+++ sprint-01/connect-uoft-frontend/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import "./styles.css"
-import "./Popup"
+import Popup from "../Popup/Popup";
 import { useState } from "react";
 
 
@@ -16,7 +16,7 @@ const Header = () => {
             <a className='profile_btn' href='/profile'>
                 <img src="/images/user_icon.svg" alt="profile icon"/>
             </a>
-            <a className='create_btn' onClick={() => setButtonPopup(true)}>Create+</a>
+            <button className='create_btn' onClick={() => setButtonPopup(true)}>Create+</button>
             <a className='manage_btn' href='/manage'>Manage</a>
 
             <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
