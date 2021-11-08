933d58f6211432543245a1cac7c2c7837a1ce94c
IanRamotar <57780992+IanRamotar@users.noreply.github.com>
2021-11-07 19:32:31 -0500

    updated README with instructions for manage and create

--- sprint-01/connect-uoft-frontend/src/views/Manage.js
+++ sprint-01/connect-uoft-frontend/src/views/Manage.js
@@ -1,4 +1,4 @@
-import postings from "../data/postings.json"
+import {postings} from "../data/data.js"
 import Components from "../components/ManagePosting/ManagePostingCreate";
 import "./Home.css"
 import SearchTag from "../components/SearchTag/SearchTag";
@@ -9,7 +9,7 @@ const Manage = () => {
 
     //currently hardcoded to only filter so that user "0" is managing
     const filteredPostings = postings.filter(function (postings) {
-        return postings.members.includes(1, 0) 
+        return postings.creator === 0 
       });
 
     return(
