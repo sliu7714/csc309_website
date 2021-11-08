b4f8d6059fd05557eae5b26ab7a701b173344a2a
Vladimir Caterov <vlad.caterov@mail.utoronto.ca>
2021-11-07 20:31:49 -0500

    updatad stats

--- sprint-01/connect-uoft-frontend/src/components/ProfileSection/Stats.js
+++ sprint-01/connect-uoft-frontend/src/components/ProfileSection/Stats.js
@@ -10,7 +10,7 @@ const Stats = (props) => {
             <h1 className="section_title">Stats</h1>
             <ul id="stats">
                 <li>Number of Courses: {user.courses.length}</li>
-                <li>Number of Groups Made: {user.groups.length}</li>
+                <li>Number of Groups Made: {user.postings.length}</li>
                 <li>Number of Groups A Member Of: {user.groups.length}</li>
             </ul>
         </div>
