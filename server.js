0fd4fb57ac687a2bfbb0f6c9546de37fc2ddb60e
sliu7714 <siyuanliu3@gmail.com>
2021-12-03 03:33:22 -0500

    add session
    note: some of the code was adapted from the react class example
    
    * signup and login routes are done
    * add helper to check if a user is an admin

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// NOTE, parts of this server setup code was adapted from the 'react-express-authentication' example we went through in class
+// NOTE: parts of this server setup code was adapted from the 'react-express-authentication' example we went through in class
 
 // To run in development mode, run normally: node server.js
 // To run in development with the test user logged in the backend, run: TEST_USER_ON=true node server.js
 // To run in production mode, run in terminal: NODE_ENV=production node server.js
 const env = process.env.NODE_ENV // read the environment variable (will be 'production' in production mode)
 
-//const USE_TEST_USER = env !== 'production' && process.env.TEST_USER_ON // option to turn on the test user.
-//const TEST_USER_ID = '5fb8b011b864666580b4efe3' // the id of our test user (you will have to replace it with a test user that you made). can also put this into a separate configutation file
-//const TEST_USER_USERNAME = 'user_test'
+const USE_TEST_USER = env !== 'production' && process.env.TEST_USER_ON // option to turn on the test user.
+const TEST_USER_ID = '61a9ba83c690a83c384c8531' // the id of our test user
 
 //setup for path macro
 const path = require('path')
@@ -35,7 +34,7 @@ const { ObjectID } = require("mongodb")
 // body-parser: middleware for parsing parts of the request into a usable object (onto req.body)
 const bodyParser = require('body-parser') 
 app.use(bodyParser.json()) // parsing JSON body
-app.use(bodyParser.urlencoded({ extended: true })); // parsing URL-encoded form data (from form POST requests)
+app.use(bodyParser.urlencoded({ extended: true })) // parsing URL-encoded form data (from form POST requests)
 
 /***************************** EXPRESS SESSION  **********************************/
 // express-session for managing user sessions
@@ -59,8 +58,8 @@ const mongoChecker = (req, res, next) => {
 }
 
 /***************************** AUTHENTICATION MIDDLEWARE **********************************/
-/**
- * const authenticate = (req, res, next) => {
+
+ const authenticate = (req, res, next) => {
     if (env !== 'production' && USE_TEST_USER)
         req.session.user = TEST_USER_ID // test user on development. (remember to run `TEST_USER_ON=true node server.js` if you want to use this user.)
 
@@ -79,17 +78,147 @@ const mongoChecker = (req, res, next) => {
         res.status(401).send("Unauthorized")
     }
 }
-**/
+
+/***************************** HELPERS **********************************/
+// should move these to seperate files later
+
+// check if the current user is a creator of a posting
+// assume user is authenticated - so req.session.user exists
+const checkIsPostingCreator = (postingID) =>{
+    // userID = req.session.user;
+    // TODO
+}
+
+// check if the current user is a creator of a posting
+// posting members does NOT include the creator of the post.
+// assume user is authenticated - so req.session.user exists
+const checkIsPostingMember = (postingID) =>{
+    // userID = req.session.user;
+    // TODO
+
+}
+
+// check if the current user is an admin user
+// assume user is authenticated - so req.session.user exists
+// make sure to await for the return value of this
+const checkIsAdmin = async (req) =>{
+
+    return User.findById(req.session.user)
+        .then(user =>{
+            if(!user){
+                console.log("checkIsAdmin: did not find user")
+                return false
+            }
+            return true
+        })
+        .catch(err =>{
+            console.log(err)
+            return false
+        })
+}
+
+/***************************** SESSION HANDLING  **********************************/
+
+// create session
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "connect uoft secret", // make a SESSION_SECRET environment variable when deploying (for example, on heroku)
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 3600000, // session lasts 1 hour
+            httpOnly: true
+        },
+        // store the sessions on the database in production
+        store: env === 'production' ? MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/ConnectUofTAPI'
+        }) : null
+    })
+);
+
+// login, create session
+app.post("/api/user/login", (req, res) =>{
+
+    const username = req.body.username
+    const password = req.body.password
+
+    // find user
+    User.findByUsernamePassword(username, password)
+        .then(user => {
+            // add user id to session
+            req.session.user = user._id
+            req.session.username = user.username
+            // make sure to update check-session endpoint as well if adding anything to this response
+            res.send({userID: user._id, isAdmin: user.isAdmin})
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).send()
+        })
+})
+
+// logout, delete session
+app.get("/api/user/logout", (req, res) =>{
+
+    // delete session
+    req.session.destroy(err => {
+        if (err){
+            res.status(500).send(err)
+        }
+        else{
+            res.send()
+        }
+    })
+})
+
+// check if user is logged in
+app.get("/api/user/check-session", async (req, res) => {
+
+    if (env !== 'production' && USE_TEST_USER){
+        req.session.user = TEST_USER_ID
+        res.send({userID: TEST_USER_ID, isAdmin: false})
+        return;
+    }
+
+    // user session exists if session.user (userid) exists -- this is set in login
+    if (req.session.user){
+        isAdmin = await checkIsAdmin(req)
+        res.send({ userID: req.session.user, isAdmin: isAdmin })
+    }
+    else{
+        res.status(401).send('No session')
+    }
+})
 
 /***************************** API ROUTES **********************************/
 
-app.get("/api/hello-world", (req, res)=>{
+// simple hello world for testing
+app.get("/api/hello-world", mongoChecker, (req, res)=>{
    res.send("hello world")
 })
 
-app.post("/api/user/create", (req, res)=>{
-    // const
-    res.send("user create TODO")
+
+// create new user
+// note: does NOT login or create a session.
+app.post("/api/user/create", mongoChecker, async (req, res)=>{
+
+    // make new user
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        name: req.body.username, // start off with username as name of the user
+        // use defaults for the rest of the properties
+    })
+
+    try{
+        const newUser = await user.save()
+        res.send(newUser)
+    }
+    catch(err){
+        res.status(400).send('Bad Request: Incorrect format or non-unique username')
+    }
+
 })
 
 
