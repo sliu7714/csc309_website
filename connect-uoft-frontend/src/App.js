0be1e2d34a2853a64e56a9580313e5f504d54aff
sliu7714 <siyuanliu3@gmail.com>
2021-12-05 21:02:55 -0500

    extract out user server calls
    * extract out user server calls for login, logout, checkSession, createUser, and getProfileInfo
    * fixed small bug with parsing non-existent body for logout response
    
    note: be careful with order of parameters to the server call helpers

--- connect-uoft-frontend/src/App.js
+++ connect-uoft-frontend/src/App.js
@@ -10,73 +10,17 @@ import Header from "./components/Header/Header";
 import Logout from "./views/Logout/Logout";
 import PostingDetailed from "./views/PostingDetailed/PostingDetailed";
 import User from "./views/User/User";
-import ENV from './config.js'
-const BASE_API_URL = ENV.apiBaseUrl
+import {checkSession, logout} from "./actions/user";
 
 function App() {
 
     const [isUserLoggedIn, setIsUserLoggedIn] = useState(true)
     const [isAdmin, setIsAdmin] = useState(false) // TODO: update components to use this boolean
 
-    const [tempCheckSession, setTempCheckSession] = useState(false) // TODO: remove
-
-
     // check if user is logged in on every refresh
     useEffect(() =>{
-        if(!ENV.useFrontendTestUser){
-            fetch(`${BASE_API_URL}/api/user/check-session`)
-                .then((res) => {
-                    if(!res.ok){
-                        console.log("check session response code:", res.status)
-                        setIsAdmin(false)
-                        setIsUserLoggedIn(false)
-                        return;
-                    }
-                    return res.json()
-                })
-                .then((sessionInfo) =>{
-                    // previous then only returns json if response code is 200
-                    if (sessionInfo){
-                        // console.log('session info', sessionInfo)
-                        setIsUserLoggedIn(true)
-                        setIsAdmin(sessionInfo.isAdmin ? sessionInfo.isAdmin : false)
-                    }
-                })
-                .catch((err) => {
-                    console.log("could not check session:",err)
-                    setIsUserLoggedIn(false)
-                })
-        }
-        else{
-            console.log('test user')
-            setIsUserLoggedIn(true)
-            setIsAdmin(ENV.frontendTestUserIsAdmin)
-        }
-
-    }, [tempCheckSession])
-
-
-    const logout = () =>{
-        console.log('logout')
-        fetch(`${BASE_API_URL}/api/user/logout`)
-            .then((res) => {
-                if(!res.ok){
-                    console.log("could not log out:", res.status)
-                    return;
-                }
-                return res.json()
-            })
-            .then((responseInfo) =>{
-                // previous then only returns json if response code is 200
-                if (responseInfo){
-                    setIsUserLoggedIn(false)
-                    setIsAdmin(false)
-                }
-            })
-            .catch((err) => {
-                console.log("could not log out:", err)
-            })
-    }
+        checkSession(setIsUserLoggedIn, setIsAdmin)
+    }, [])
 
 
   return (
@@ -110,7 +54,7 @@ function App() {
                 </Route>
 
                 <Route path="/logout">
-                    <Logout logout={logout} isUserLoggedIn={isUserLoggedIn}/>
+                    <Logout logout={() => logout(setIsUserLoggedIn, setIsAdmin)} isUserLoggedIn={isUserLoggedIn}/>
                 </Route>
 
                 <Route path="/posting/:id">
@@ -128,8 +72,7 @@ function App() {
         </BrowserRouter>
 
         {/*TEMPORARY*/}
-        {/*<button onClick={logout}>logout (temporary - need additional refresh) isAdmin: {isAdmin}</button>*/}
-        {/*<button onClick={() => setTempCheckSession(!tempCheckSession)}>check session </button>*/}
+        {/*<button onClick={() => logout(isUserLoggedIn, isAdmin)}>logout (temporary - need additional refresh) isAdmin: {isAdmin}</button>*/}
         {/*<button >isUserLoggedIn {isUserLoggedIn.toString()} </button>*/}
     </div>
   );
---
+++ connect-uoft-frontend/src/actions/user.js
@@ -0,0 +1,150 @@
+import ENV from "../config";
+const BASE_API_URL = ENV.apiBaseUrl
+
+// note: careful with order of parameters
+
+export const checkSession = (setIsUserLoggedIn, setIsAdmin) => {
+    if(!ENV.useFrontendTestUser){
+        fetch(`${BASE_API_URL}/api/user/check-session`)
+            .then((res) => {
+                if(!res.ok){
+                    console.log("check session response code:", res.status)
+                    setIsAdmin(false)
+                    setIsUserLoggedIn(false)
+                    return;
+                }
+                return res.json()
+            })
+            .then((sessionInfo) =>{
+                // previous then only returns json if response code is 200
+                if (sessionInfo){
+                    // console.log('session info', sessionInfo)
+                    setIsUserLoggedIn(true)
+                    setIsAdmin(sessionInfo.isAdmin ? sessionInfo.isAdmin : false)
+                }
+            })
+            .catch((err) => {
+                console.log("could not check session:",err)
+                setIsUserLoggedIn(false)
+            })
+    }
+    else{
+        console.log('test user')
+        setIsUserLoggedIn(true)
+        setIsAdmin(ENV.frontendTestUserIsAdmin)
+    }
+}
+
+export const logout = (setIsUserLoggedIn, setIsAdmin) => {
+    console.log('logout')
+    fetch(`${BASE_API_URL}/api/user/logout`)
+        .then((res) => {
+            if(!res.ok){
+                console.log("could not log out:", res.status)
+                alert("server issue, could not log out")
+                return;
+            }
+            setIsUserLoggedIn(false)
+            setIsAdmin(false)
+        })
+        .catch((err) => {
+            console.log("could not log out:", err)
+        })
+}
+
+export const login = (username, password, setIsIncorrectCredentials, setIsUserLoggedIn, setIsAdmin, setPassword) =>{
+
+    fetch(`${BASE_API_URL}/api/user/login`,{
+        method: "post",
+        body: JSON.stringify({username, password}),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    })
+        .then((res) =>{
+            if(!res.ok){
+                console.log("invalid login: response code ", res.status)
+                setIsIncorrectCredentials(true)
+                return;
+            }
+            return res.json()
+        })
+        .then((responseInfo) =>{
+            if (responseInfo){
+                // console.log(responseInfo)
+                setIsUserLoggedIn(true)
+                setIsAdmin(responseInfo.isAdmin ? responseInfo.isAdmin : false)
+                setPassword("")
+            }
+        })
+        .catch((err) =>{
+            console.log("error with logging in: ", err)
+        })
+}
+
+const validateEmail = (email) =>{
+    // RFC 5322 standard email regex from http://emailregex.com/
+    return email.match('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
+}
+
+export const signup = (username, password, email, setMessage, setPassword, passwordLengthMessage, invalidEmailMessage, nonUniqueUsernameMsg, successSignupMessage, signupErrorMsg) =>{
+    setMessage(null)
+
+    if (password.length < 4){
+        setMessage(passwordLengthMessage)
+        return;
+    }
+    if (!validateEmail(email)){
+        setMessage(invalidEmailMessage)
+        return;
+    }
+
+    fetch(`${BASE_API_URL}/api/user/create`,{
+        method: "post",
+        body: JSON.stringify({username, password, email}),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    })
+        .then((res) =>{
+            if(!res.ok){
+                console.log("invalid signup, response code: ", res.status)
+                setMessage(nonUniqueUsernameMsg)
+                return;
+            }
+            return res.json()
+        })
+        .then((responseInfo) =>{
+            if (responseInfo){
+                console.log(responseInfo)
+                setMessage(successSignupMessage(username))
+                setPassword("")
+            }
+        })
+        .catch((err) =>{
+            console.log("error with signing up in: ", err)
+            setMessage(signupErrorMsg)
+        })
+}
+
+export const getProfileInfo = (setUser) =>{
+    fetch(`${BASE_API_URL}/api/user`)
+        .then((res) =>{
+            if(!res.ok){
+                console.log("could not find user ", res.status)
+                return;
+            }
+            return res.json()
+        })
+        .then((userInfo) =>{
+            if (userInfo){
+                console.log(userInfo)
+                setUser(userInfo)
+            }
+        })
+        .catch((err) =>{
+            console.log("error with getting profile info: ", err)
+        })
+}
